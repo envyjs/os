230de2f1156076dbff20f280fba73be1df1bc16b
OwnedByWuigi <120682622+OwnedByWuigi@users.noreply.github.com>
2023-11-04 18:40:55 +0000

    Add files via upload

---
+++ README.md
@@ -0,0 +1,23 @@
+![Envy logo](envy.jpeg)
+
+## Envy, the open OS
+Envy is a environment for locked down devices, such as Chromebooks or Windows devices.
+
+## How do I access Envy?
+Via [https://envywebos.github.io](https://envywebos.github.io/).
+
+## What can Envy do?
+**THIS PART OF THE README IS DUE TO CHANGE.**
+
+  Envy can do multiple things, including:
+  
+  * Alpine Linux *(via JS/Linux!)*
+  * Basic gaming (Anything HTML5/js)
+  * Browse the web, with Project Aspen!
+  * Customize itself, with wallpapers!
+  * Discord *(via Discross!)*
+  * Get new apps with the Envy Store!
+  * x86 Emulation! 
+
+![Envy desktop](45preview.png)
+
---
+++ storeid.js
@@ -0,0 +1,154 @@
+function id1() {
+  new WinBox({
+    title: "Piskel for Envy",
+    class: "eclipsemono",
+    x: "center",
+    y: "center",
+    width: "50%",
+    height: "50%",
+    top: 0,
+    right: 0,
+    bottom: 45,
+    left: 0,
+    url: "https://www.piskelapp.com/p/create/sprite",
+  });
+}
+
+function id2() {
+  new WinBox({
+    title: "Localhost Viewer",
+    class: "eclipsemono",
+    x: "center",
+    y: "center",
+    width: "50%",
+    height: "50%",
+    top: 0,
+    right: 0,
+    bottom: 45,
+    left: 0,
+    url: "http://localhost:3000",
+  });
+}
+
+function id3() {
+  let appName = "Aspen Browser",
+    appIcon = "icons/aspen.png";
+  let wb = new WinBox({
+    title: appName,
+    icon: "icons/aspen.png",
+    class: ["eclipsemono"],
+    x: "center",
+    y: "center",
+    width: "50%",
+    height: "50%",
+    top: 0,
+    right: 0,
+    bottom: 45,
+    left: 0,
+    mount: document.getElementById("TeresaMark").cloneNode(true),
+  });
+
+  addTaskbarItem(appName, appIcon, wb);
+}
+
+function id4() {
+  new WinBox({
+    title: "Eaglercraft",
+    class: "eclipsemono",
+    x: "center",
+    y: "center",
+    width: "50%",
+    height: "50%",
+    top: 0,
+    right: 0,
+    bottom: 45,
+    left: 0,
+    url: "https://eaglercraft.com/",
+  });
+}
+
+function id5() {
+  new WinBox({
+    title: "Resent Client",
+    class: "eclipsemono",
+    x: "center",
+    y: "center",
+    width: "50%",
+    height: "50%",
+    top: 0,
+    right: 0,
+    bottom: 45,
+    left: 0,
+    url: "https://reslauncher.vercel.app/",
+  });
+}
+
+function id6() {
+  new WinBox({
+    title: "Calculator",
+    class: "eclipsemono",
+    x: "center",
+    y: "center",
+    width: "27%",
+    height: "70%",
+    top: 0,
+    right: 0,
+    bottom: 45,
+    left: 0,
+    url: "http://freshman-calculator.surge.sh/",
+  });
+}
+
+function id7() {
+  new WinBox({
+    title: "Virtual x86",
+    class: "eclipsemono",
+    x: "center",
+    y: "center",
+    width: "27%",
+    height: "70%",
+    top: 0,
+    right: 0,
+    bottom: 45,
+    left: 0,
+    url: "https://copy.sh/v86/",
+  });
+}
+
+function id8() {
+  new WinBox({
+    title: "Notepad Legacy",
+    class: "eclipsemono",
+    x: "center",
+    y: "center",
+    width: "27%",
+    height: "70%",
+    top: 0,
+    right: 0,
+    bottom: 45,
+    left: 0,
+    url: "https://zen.unit.ms/",
+  });
+}
+
+function id9() {
+  let appName = "Maps (Beta)",
+    appIcon = "https://static.vecteezy.com/system/resources/previews/016/716/478/original/google-maps-icon-free-png.png";
+
+  let wb = new WinBox({
+    title: appName,
+    icon: appIcon,
+    class: ["no-title", "eclipsemono"],
+    x: "center",
+    y: "center",
+    width: "50%",
+    height: "50%",
+    top: 0,
+    right: 0,
+    bottom: 45,
+    left: 0,
+    url: "https://maps.google.com/maps?width=100%25&amp;height=200&amp;hl=en&amp;q=1+()&amp;t=&amp;z=1&amp;ie=UTF8&amp;iwloc=B&amp;output=embed",
+  });
+
+  let index = addTaskbarItem(appName, appIcon, wb);
+}
