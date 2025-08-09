917c4cf956dbd1ef591a8227c308ffe768426f8b
ownedbywuigi <ownedbywuigi@gmail.com>
2025-08-09 18:15:31 +0100

    Fully integration with both Cherry Tree and eWinBox, no more need for Legacy Mode

--- API/System.LegacyModeIn8.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    const windowTitle = 'Legacy mode (8.0.1)';
-    const customHTML = `
-    <iframe id="emb" src="../Subsystems/legacy/index.html">
-    `;
-  
-    // Create a window dynamically
-    createWindow(windowTitle, customHTML, 800, 600);
-  })();
