fd46d0ae53220c0ee5deb5397e1a13615e178f04
ownedbywuigi <ownedbywuigi@gmail.com>
2025-09-06 19:46:10 +0100

    [3006.11] Start work on Settings revamp

--- Apps/Settings.js
+++ Apps/Settings.js
@@ -1,8 +1,22 @@
 (function() {
   const windowTitle = 'Settings';
   const customHTML = `
-  <h2>Appearence</h2>
-  <button onclick="loadScript('/Libs/spotlight.js')">Envy Spotlight</button>
+  <div style="display: flex;">
+    <div style="width: 30%">
+    <button style="min-width: 87%; height: 80px; text-align: left; line-height: 5px; display: flex;">
+      <div style="width: 25%; margin-top: 24px">
+      <img src="./Assets/settings/brush.svg">
+      </div>
+      <div style="width: 75%;">
+      <h4>Appearence</h4>
+      <p style="font-size: 13px;">Customize Envy</p>
+      </div>
+    </button>
+    </div>
+    <div style="width: 70%">
+      <h2>Appearence</h2>
+      <button onclick="loadScript('/Libs/spotlight.js')">Envy Spotlight</button>
+    </div>
   `;
   createWindow(windowTitle, customHTML, 600, 350);
 })();
