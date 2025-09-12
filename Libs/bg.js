2f3ee4e4fd7bae5ea709fe60e460a818b1afe468
OwnedByWuigi <ownedbywuigi@gmail.com>
2025-09-12 10:54:17 +0100

    prepare for 2026 update

--- Libs/bg.js
+++
@@ -1,6 +0,0 @@
-var content = document.createElement("div");
-content.innerHTML = `
-<div id="bg">
-</div>
-`;
-document.getElementById("userland").appendChild(content);
--- Libs/spotlight.js
+++
@@ -1,9 +0,0 @@
-// Envy Spotlight daily wallpaper service
-
-var content = document.createElement("div");
-content.innerHTML = `
-<div id="spotlight">
-    <img src="https://picsum.photos/2000">
-</div>
-`;
-document.getElementById("userland").appendChild(content);
