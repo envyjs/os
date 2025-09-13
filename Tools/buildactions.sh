99abe6c7b43e803b333be755c4998b0b62b5df49
OwnedByWuigi <ownedbywuigi@gmail.com>
2025-09-13 13:00:55 +0100

    Update buildactions

--- Tools/buildactions.sh
+++ Tools/buildactions.sh
@@ -3,13 +3,13 @@
 # Envy Kernel build script for GitHub Actions
 
 # Make all script files in the Tools directory executable (*nix only)
-chmod +x ./*.sh
-chmod +x ./Compilation/*.sh
+chmod +x ./Tools/*.sh
+chmod +x ./Tools/Compilation/*.sh
 
 # Make a output folder for the compil
-mkdir ../Output/
-mkdir ../Output/bin/
-mkdir ../Output/install/
+mkdir ./Output/
+mkdir ./Output/bin/
+mkdir ./Output/install/
 
 sudo apt update
 sudo apt install -y nasm
