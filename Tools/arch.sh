8c8da84c3c894db9783bb0b1e24d65a3aa45fbb5
ownedbywuigi <ownedbywuigi@gmail.com>
2025-10-02 22:04:02 +0100

    AOSP source retrival kit for Envy Kernel

--- Tools/arch.sh
+++ Tools/arch.sh
@@ -10,6 +10,7 @@ show_menu() {
     echo "1. Build for i386"
     echo "2. Build for AMD64"
     echo "3. Build for armv8 (WIP)"
+    echo "4. Build for armv8 with Android (WIP)"
     echo""
     echo "Enter a number from the list: "
 }
@@ -35,6 +36,22 @@ while true; do
             exec "./Compilation/armv8.sh"
             break
             ;;
+        4)
+            cd ../System/armv8/Compat/
+            echo "!! CAUTION !!"
+            echo "This will download a large amount of data (around 100GB) from the Android source repository."
+            echo "Make sure you have sufficient disk space and a stable internet connection."
+            echo "This process may take a long time (up to 5 hours!) depending on your internet speed."
+            read -p "Continue? (y/n): " yn
+            case $yn in
+                [Yy]* ) 
+                exec repo init -u https://android.googlesource.com/platform/manifest -b android-14.0.0_r30
+                exec repo sync -j$(nproc)
+                exec "./Compilation/armv8.sh"
+                ;;
+                [Nn]* ) echo "Aborted."; exit 1 ;;
+                * ) echo "Please answer yes or no."; exit 1 ;;
+            esac
         *)
             echo "Invalid choice. Returning to menu."
             ;;
